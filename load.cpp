8efa0caf789694cb26c3f8af69949f1f07a633fd
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-03-29 20:31:24 -0400

    Update load.cpp

--- load.cpp
+++ load.cpp
@@ -1,10 +1,16 @@
 #include <lua.hpp>
+const char *bytecode = ...; // pointer to the bytecode buffer
+size_t bytecode_size = ...; // size of the bytecode buffer
+const char *chunkname = "mychunk"; // name of the chunk (used for debugging/error messages)
+const char *mode = "b"; // the mode of the chunk ("b" for binary)
+int error = lua_load(L, (const unsigned char *) bytecode, bytecode_size, chunkname, mode);
 
+//only luac not luau skid
 int main() {
     lua_State *L = luaL_newstate(); // create a new Lua state
-    luaL_openlibs(L); // load standard Lua libraries
+    luaL_openlibs(L); //get em libs
 
-    // assume the bytecode is stored in a buffer named 'bytecode'
+    
     size_t bytecode_size = ...; // the size of the bytecode buffer
     int error = lua_load(L, bytecode, bytecode_size, "mychunk", "b");
     if (error) {
