35265a51136498634b7ffef99437d618fe5a54e4
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-03-30 16:25:53 -0400

    Update load.cpp

--- load.cpp
+++ load.cpp
@@ -1,43 +1,33 @@
+//can load lua bytecode
 #include <lua.hpp>
-const char *bytecode = ...; // pointer to the bytecode buffer
-size_t bytecode_size = ...; // size of the bytecode buffer
-const char *chunkname = "mychunk"; // name of the chunk (used for debugging/error messages)
-const char *mode = "b"; // the mode of the chunk ("b" for binary)
-int error = lua_load(L, (const unsigned char *) bytecode, bytecode_size, chunkname, mode);
-
+const char* bytecode = "NIGGER";//bytecode
 //only luac not luau skid
 int main() {
-    lua_State *L = luaL_newstate(); // create a new Lua state
+    lua_State *L = luaL_newstate(); // create a new  state
     luaL_openlibs(L); //get em libs
 
     
-    size_t bytecode_size = ...; // the size of the bytecode buffer
-    int error = lua_load(L, bytecode, bytecode_size, "mychunk", "b");
+    size_t bytecode_size = ...; // the size of the bytecode
+    int error = lua_load(L, bytecode, bytecode_size, "Axon", "b");
     if (error) {
         const char *error_msg = lua_tostring(L, -1);
-        // handle the error, e.g. print the error message
+        //TODO: if luau skidsploit then print it out 
         lua_close(L);
         return 1;
     }
 
-    // the bytecode is loaded into a function on top of the stack
-    // you can call this function with lua_pcall or lua_call
-    int nargs = 0; // number of arguments to the function
-    int nresults = 0; // number of results to expect
-    error = lua_pcall(L, nargs, nresults, 0);
+    error = lua_pcall(L, 0x0, 0x0, 0);
     if (error) {
         const char *error_msg = lua_tostring(L, -1);
-        // handle the error, e.g. print the error message
+        //TODO: skidsploit must return with getglobal and pushstring
         lua_close(L);
         return 1;
     }
 
-    // the results of the function are on top of the stack
-    // you can manipulate them with lua_to* functions
-    // e.g. to get a string result:
+    // the results of the function 
     const char *result = lua_tostring(L, -1);
-    // do something with the result
+    //do whatever LOL u got result now
 
-    lua_close(L); // free the Lua state
+    lua_close(L); // free me
     return 0;
 }
